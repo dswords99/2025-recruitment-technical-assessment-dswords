73c50f9612c3d2849a4577330e64b934ddcc777a
daniel <daniel.b.swords@gmail.com>
2025-02-10 14:06:11 +1100

    done some of task

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = []
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,16 +43,61 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+	
+	# From what I can tell, this code should be correct via manual test
+	# Not too sure why my tests aren't passing, could be my machine
+	# Or something I am overlooking with the servers.
+	# I'm on a pretty fresh install of linux and had to install and fix some stuff
+	# with environments and so on so not sure if that affects it.
+
+	add_whitespace = "".join(map(lambda c: " " if c in "_-" else c, recipeName))
+	remove_nonchar = "".join([c for c in add_whitespace if c.isalpha() or c == " "])
+
+	words = remove_nonchar.split()
+	capitalised = " ".join(map(lambda w: w.capitalize(), words))
+
+	if len(capitalised) == 0:
+		return None
+
+	return capitalised
 
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	data = request.get_json()
+
+	if data.name in map(lambda c_ent: c_ent.name, cookbook):
+		'entry names must be unique', 400
+
+	if data.type == "recipe":
+		items = []
+
+		for r_item in data.requiredItems:
+			if r_item.name in map(lambda i: i.name, items):
+				return 'Recipe requiredItems can only have one element per name', 400
+			
+			items.append(RequiredItem(r_item.name, r_item.quantity))
+
+
+		recipe = Recipe(data.name, items)
+		cookbook.append(recipe)
+		
+	elif data.type == "ingredient":
+		if data.cookTime < 0:
+			return 'cook time must be >= 0', 400
+		
+		ing = Ingredient(data.name, data.cook_time)
+		cookbook.append(ing)
+	else:
+		return 'type must be recipe or ingredient', 400
+	
+
+	
+
+
+	return 'success', 200
 
 
 # [TASK 3] ====================================================================
